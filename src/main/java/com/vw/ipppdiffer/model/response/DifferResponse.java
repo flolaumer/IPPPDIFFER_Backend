9361625af33548034b6c1ab4597410df5eea13f2
FlorinSingeorzan <Florin.Singeorzan@computacenter.com>
2021-11-03 15:48:44 +0000

    Differ Implementation

--- src/main/java/com/vw/ipppdiffer/model/response/DifferResponse.java
+++ src/main/java/com/vw/ipppdiffer/model/response/DifferResponse.java
@@ -1,10 +1,12 @@
 package com.vw.ipppdiffer.model.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class DifferResponse {
     private Element firstTree;
     private Element secondTree;
