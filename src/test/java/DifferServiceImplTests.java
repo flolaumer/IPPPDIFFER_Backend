36dbb3592e6a86217f5f7aa10cf432c12e04097c
Kristof <Anamaria.Kristof@computacenter.com>
2021-11-09 15:31:22 +0200

    Added suggested improvements

--- src/test/java/DifferServiceImplTests.java
+++ src/test/java/DifferServiceImplTests.java
@@ -1,33 +1,60 @@
+import com.vw.ipppdiffer.model.response.DifferResponse;
 import com.vw.ipppdiffer.model.response.Element;
 import com.vw.ipppdiffer.service.DifferServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class DifferServiceImplTests {
-    DifferServiceImpl service = new DifferServiceImpl();
-    MockMultipartFile firstFile = new MockMultipartFile("IB100FF.xml", "IB100FF.xml", "text/xml",
-            this.getClass().getClassLoader()
-                    .getResourceAsStream("IB100FF.xml"));
-    MockMultipartFile secondFile = new MockMultipartFile("IB100FF2.xml", "IB100FF2.xml", "text/xml",
-            this.getClass().getClassLoader()
-                    .getResourceAsStream("IB100FF2.xml"));
+class DifferServiceImplTests {
+    private DifferServiceImpl service;
+    private static final String FIRST_FILE = "IB100FF.xml";
+    private static final String SECOND_FILE = "IB100FF2.xml";
+    private MockMultipartFile firstFile;
+    private MockMultipartFile secondFile;
 
-    public DifferServiceImplTests() throws IOException {
+    @BeforeEach
+    public void setup() throws IOException {
+        service = new DifferServiceImpl();
+        firstFile = new MockMultipartFile(FIRST_FILE, FIRST_FILE, "text/xml",
+                this.getClass().getClassLoader()
+                        .getResourceAsStream(FIRST_FILE));
+        secondFile = new MockMultipartFile(SECOND_FILE, SECOND_FILE, "text/xml",
+                this.getClass().getClassLoader()
+                        .getResourceAsStream(SECOND_FILE));
     }
 
     @Test
-    void canParseValidTree() {
+    void parseValidTree() {
         Element element = service.getTreeStructure(firstFile);
-        assertTrue(element != null);
+        assertNotNull(element);
+        assertEquals("IB-1", element.getName());
+        assertEquals("#000000", element.getColor());
+        assertEquals(7, element.getChildren().size());
+        assertEquals(1, element.getAttributes().size());
     }
 
     @Test
     void canGetValidDiffer() {
-        Object getDiffer = service.getDiffer(firstFile, secondFile);
-        assertTrue(getDiffer != null);
+        DifferResponse getDiffer = service.getDiffer(firstFile, secondFile);
+        Element firstTree = getDiffer.getFirstTree();
+        Element secondTree = getDiffer.getSecondTree();
+        assertNotNull(getDiffer);
+        assertNotNull(firstTree);
+        assertNotNull(secondTree);
+        assertEquals("IB-1", firstTree.getName());
+        assertEquals("#FF9B00", firstTree.getColor());
+        assertEquals(7, firstTree.getChildren().size());
+        assertEquals(1, firstTree.getAttributes().size());
+        assertEquals("IB-1", secondTree.getName());
+        assertEquals("#FF9B00", secondTree.getColor());
+        assertEquals(7, secondTree.getChildren().size());
+        assertEquals(1, secondTree.getAttributes().size());
+
+
     }
 }
